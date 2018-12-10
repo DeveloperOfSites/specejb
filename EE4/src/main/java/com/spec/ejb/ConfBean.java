82599e5c1e41f84b465482e7375035cd7d4b3f00
DeveloperOfSites <DeveloperOfSites@Yandex.Ru>
2018-12-10 23:12:39 +0700

    Connection my local db

--- EE4/src/main/java/com/spec/ejb/ConfBean.java
+++ EE4/src/main/java/com/spec/ejb/ConfBean.java
@@ -5,6 +5,11 @@
  */
 package com.spec.ejb;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Singleton;
@@ -34,6 +39,27 @@ public class ConfBean {
     private void init() {
         System.out.println("siteURL = "+siteURL);
         System.out.println("jndiCustomTest = "+test);
+        
+        try {
+            Connection con = dataSource.getConnection();
+            String sql = "select * from public.test1"; // coursetotals where spid=?
+            ResultSet rs = con.prepareStatement(sql).executeQuery();
+            //statement = connection.prepareStatement(sql);
+            //statement.setLong(1, sid);
+            //rs = statement.executeQuery();
+            //for every row, call read method to extract column 
+            //values and place them in a coursesummary instance
+            while (rs.next()) {
+                System.out.println("" + rs.getRow() + ". " + "id = " + rs.getInt("uid") + ", name = '" + rs.getString("str1"));
+            }
+//                CourseSummary coursesummary = read("findAll", rs);
+//                coursesummaries.add(coursesummary);
+//            }
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(ConfBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
     public String getURrL() {
